446fba3dd1c0562c6a46b790d115d5638598bfbe
Mauricio Rojas <mrojasb2000@gmail.com>
2019-02-25 12:12:49 -0300

    Remove target folder.

--- chapter07/communicator/target/debug/deps/communicator-2db05afffe83818f.d
+++
@@ -1,8 +0,0 @@
-/Users/mavro/Documents/Projects/Rust/rust-programming-lang/chapter07/communicator/target/debug/deps/libcommunicator-2db05afffe83818f.rlib: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
-
-/Users/mavro/Documents/Projects/Rust/rust-programming-lang/chapter07/communicator/target/debug/deps/communicator-2db05afffe83818f.d: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
-
-src/lib.rs:
-src/client.rs:
-src/network/mod.rs:
-src/network/server.rs:
--- chapter07/communicator/target/debug/deps/communicator-e66dbe47bfe54ded.d
+++
@@ -1,5 +0,0 @@
-/Users/mavro/Documents/Projects/Rust/rust-programming-lang/chapter07/communicator/target/debug/deps/communicator-e66dbe47bfe54ded: src/main.rs
-
-/Users/mavro/Documents/Projects/Rust/rust-programming-lang/chapter07/communicator/target/debug/deps/communicator-e66dbe47bfe54ded.d: src/main.rs
-
-src/main.rs:
