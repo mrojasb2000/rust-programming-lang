41c3d71becc2f70fb739a7ad302840d13cd4e6ea
Mauricio Rojas <mrojasb2000@gmail.com>
2019-02-26 23:20:27 -0300

    Adds bringing names into scope with the use keyword.

--- chapter07/communicator/src/lib.rs
+++ chapter07/communicator/src/lib.rs
@@ -2,6 +2,34 @@ pub mod client;
 
 pub mod network;
 
+/*
+mod outermost {
+	pub fn middle_function(){}
+	
+	fn middle_secret_function(){}
+	
+	mod inside {
+		pub fn inner_function(){}
+		fn secret_function(){}
+	}
+}
+
+
+fn try_me(){
+	outermost::middle_function();
+	outermost::middle_secret_function();
+	outermost::inside::inner_function();
+	outermost::inside::secret_function();
+}*/
+
+pub mod a {
+	pub mod series {
+		pub mod of {
+			pub fn nested_modules(){}
+		}
+	}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
--- chapter07/communicator/src/main.rs
+++ chapter07/communicator/src/main.rs
@@ -1,7 +1,16 @@
 extern crate communicator;
 
+//use communicator::a::series::of;
+use communicator::a::series::of::nested_modules;
+
 fn main() {
     communicator::client::connect();
     communicator::network::connect();
     communicator::network::server::connect();
+	
+	// Calling a qualified module name
+	communicator::a::series::of::nested_modules();
+	
+	//of::nested_modules();
+	nested_modules();
 }
