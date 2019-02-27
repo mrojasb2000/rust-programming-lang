b65de92e39ab5594897c8dd1cf4591d7d42f5009
Mauricio Rojas <mrojasb2000@gmail.com>
2019-02-27 11:10:27 -0300

    Adds Referring to names in different modules.

--- chapter07/communicator/src/lib.rs
+++ chapter07/communicator/src/lib.rs
@@ -2,26 +2,6 @@ pub mod client;
 
 pub mod network;
 
-/*
-mod outermost {
-	pub fn middle_function(){}
-	
-	fn middle_secret_function(){}
-	
-	mod inside {
-		pub fn inner_function(){}
-		fn secret_function(){}
-	}
-}
-
-
-fn try_me(){
-	outermost::middle_function();
-	outermost::middle_secret_function();
-	outermost::inside::inner_function();
-	outermost::inside::secret_function();
-}*/
-
 pub mod a {
 	pub mod series {
 		pub mod of {
